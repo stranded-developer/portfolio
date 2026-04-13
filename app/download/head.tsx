280a40cd738d28dfc0b6d3dd9c19a86d2de9414f
Wilson Husen <wilsonhusen78@gmail.com>
2026-04-13 14:01:00 +0700

    qr

---
+++ app/download/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Download Eatzy</title>
+      <meta
+        name="description"
+        content="Download Eatzy on the App Store or Google Play."
+      />
+      <meta name="robots" content="noindex, nofollow" />
+    </>
+  );
+}
